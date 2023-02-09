8d8b30516b98a2ddf1a242b54a9e35af8e9cf38e
McDinii <denisnechay@gmail.com>
2023-02-09 12:24:14 +0300

    remove api jwt urls

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 
@@ -17,4 +18,7 @@ router.register('add/video', AdvancedRegistration_Video_ApiView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
